c49d814056256c66e48a81c80c83cc78f69caea6
Yuichi Araki <yaraki@google.com>
2016-07-07 13:47:18 +0900

    PdfRendererBasic: Fix lint errors
    Also better Material Design.
    
    Change-Id: I915e46e7ba17a9f4caffca161bee6dd6c971f034

--- ui/graphics/PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/PdfRendererBasicFragment.java
+++ ui/graphics/PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/PdfRendererBasicFragment.java
@@ -17,12 +17,12 @@
 package com.example.android.pdfrendererbasic;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
