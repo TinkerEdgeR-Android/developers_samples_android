8a6fab5ee40c42feb713a2d5e7ac38d7439606dc
Jeremy Walker <jewalker@google.com>
2018-07-27 18:10:34 -0700

    Convert to androidx and update copyright year.
    Bug: 111830148
    Test: Manually tested.
    Change-Id: I01aab6b064f1e4d255372a068b31ee67570ccfff

--- connectivity/wifirtt/WifiRttScan/Application/src/main/java/com/example/android/wifirttscan/LocationPermissionRequestActivity.java
+++ connectivity/wifirtt/WifiRttScan/Application/src/main/java/com/example/android/wifirttscan/LocationPermissionRequestActivity.java
@@ -19,9 +19,9 @@ import android.Manifest;
 import android.Manifest.permission;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
